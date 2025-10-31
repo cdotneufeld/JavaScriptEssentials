6e9f0c9f8ffe4d26cf4163ffe938336132c8d871
Christopher Neufeld <neufeldc@hotmail.com>
2025-10-31 16:13:22 +0000

    added a check when 5 and 10 followers are hit to send an alert.

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,8 +3,17 @@ let count = 0;
 function increaseCount() {
     count++;
     displayCount();
+    checkCountValue();
 }
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Congratulations! You've reached 10 followers!");
+    } else if (count === 5) {
+        alert("Great job! You've reached 5 followers!");
+    }
 }
